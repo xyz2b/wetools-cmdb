60c669a69552d0fb4d3f1a4e76f1300cb8918eb9
xyz2b <jiaohongyua@gmail.com>
2022-10-14 16:52:47 +0800

    sync itsm problem

--- src/main/java/com/webank/wetoolscmdb/cron/SyncItsmProblemsDataProcessor.java
+++ src/main/java/com/webank/wetoolscmdb/cron/SyncItsmProblemsDataProcessor.java
@@ -3,7 +3,6 @@ package com.webank.wetoolscmdb.cron;
 import com.webank.wetoolscmdb.config.ItsmApiProperties;
 import com.webank.wetoolscmdb.constant.consist.CmdbApiConsist;
 import com.webank.wetoolscmdb.model.dto.itsm.ItsmProblemsResponse;
-import com.webank.wetoolscmdb.service.intf.CmdbService;
 import com.webank.wetoolscmdb.service.intf.ItsmProblemsService;
 import com.webank.wetoolscmdb.utils.itsm.ItsmApiUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +15,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-// TODO: 定期同步ITSM问题单，根据上一次同步问题单中的最晚创建时间作为时间节点一直到当前时间，根据创建时间进行过滤。
+//  定期同步ITSM问题单，根据上一次同步问题单中的最晚创建时间作为时间节点一直到当前时间，根据创建时间进行过滤。
 //  如果当前没有同步过问题单，库里的记录数为0，就同步当前时间这一年内的问题单
 @Component
 public class SyncItsmProblemsDataProcessor implements BasicProcessor {
